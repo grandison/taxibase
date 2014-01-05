00c0e11cbf4661cbca10529649cef786b85b3d4a
Alexander Osmanov <grandison@mail.ru>
2014-01-05 19:58:04 +0400

    Fix scopes

--- app/models/taxist.rb
+++ app/models/taxist.rb
@@ -1,8 +1,8 @@
 class Taxist < ActiveRecord::Base
   attr_accessible :address, :first_name, :last_name, :photo, :third_name, :checked, :user_id
   mount_uploader :photo, PhotoUploader
-  scope :checked, where("checked = 1")
-  scope :not_checked, where("checked = 0")
+  scope :checked, where("checked = true")
+  scope :not_checked, where("checked = false")
 
   validates :first_name, presence: true
 
